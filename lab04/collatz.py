f981c37a02cb34a5d63110725c6f376ee89d8c63
Damian Sepielak <damian.sepielak@gmail.com>
2015-12-07 23:28:20 +0100

    collatz demonstration file

---
+++ lab04/collatz.py
@@ -0,0 +1,17 @@
+__author__ = 'damian'
+
+c = 65735346575434578
+itera = 0
+
+def step(c, div, mul, add):
+    if c % 2 == 0:
+        print("{}\t{}\t{}".format(c/div, int(c/div), c//div))
+        return int(c / div)
+    else:
+        return mul * c + add
+
+while c > 1:
+    c = step(c, 2, 3, 1)
+    itera = itera + 1
+
+print(itera)
